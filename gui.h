4bfe45308810480aefb33cc3ca0f5249b889f2dd
PipatPipatPong <146576748+PPpooh010@users.noreply.github.com>
2023-10-03 01:36:26 +0700

    Update gui.h

--- gui.h
+++ gui.h
@@ -17,11 +17,26 @@ public:
 	sf::RectangleShape player2StaminaBarBG;
 	sf::RectangleShape player2StaminaBar;
 
+	int mins;
+	int secs;
+	int pre_mins;
+	int pre_secs;
+
+
+	sf::Text min; 
+	sf::Text pre_min;
+	sf::Text sec;
+	sf::Text pre_sec;
+
+	sf::Text midmid ;
+	sf::Font font_str;
+	sf::Clock count_time;
+
 
 	gui();
 
 	void Init();
-	void Update(int player1HP, int player2HP , float Stamina_player1 , float Stamina_player2);
+	void Update(int player1HP, int player2HP , float Stamina_player1 , float Stamina_player2 );
 	void Draw(sf::RenderWindow& window);
 };
 
